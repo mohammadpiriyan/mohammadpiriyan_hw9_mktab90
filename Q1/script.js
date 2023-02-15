1c0c43b299c535b189fa2d4bd3625db05575703a
mohammadpiriyan <50291430+mohammadpiriyan@users.noreply.github.com>
2023-02-15 21:55:34 +0330

    end

--- Q1/script.js
+++ Q1/script.js
@@ -1,21 +1,21 @@
 
-//1
+
 const table = document.querySelector("#age-table");
 console.log(table);
-//2
+
 const labels = table.querySelectorAll("label");
 console.log(labels);
-//3
+
 const age = table.querySelector("td:first-child").innerHTML;
 console.log(age);
 
-//4
+
 const form = document.querySelector('[name="search-person"]');
 console.log(form);
-//5
+
 const firstInput = form.querySelector("input:first-child");
 console.log(firstInput);
-//6
+
 const lastInputs = form.querySelectorAll("input:last-child");
 const lastInput = lastInputs[lastInputs.length - 1];
 console.log(lastInput);
