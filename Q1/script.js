7bf9d3cda87c7dcdddf8a29fc512a4a160157b52
mohammadpiriyan <50291430+mohammadpiriyan@users.noreply.github.com>
2023-02-15 15:04:38 +0330

    end task 6

--- Q1/script.js
+++ Q1/script.js
@@ -1,12 +1,4 @@
-console.log("mamad");
-let elem = document.getElementById("elem");
-console.log(elem.children);
-
-clear = (elem) => {
-  console.log("salam");
-  for (let i = 0; i < elem.childNodes.length; i++) {
-    console.log(elem.childNodes[i]);
-    elem.childNodes[i].innerHTML = "";
-  }
+document.getElementById("hider").onclick = () => {
+  document.getElementById("text").hidden = true;
+  document.getElementById("hider").hidden = true;
 };
-clear(elem);
